96ad5e1bf8cdae90b66f7c459229835a71798636
zenwarr <zenw@yandex.ru>
2020-10-21 00:03:43 +0600

    fix: file name

--- lib/Builder.ts
+++ lib/Builder.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 import * as fs from "fs";
-import {ManifestReader} from "./ManifestUpdater";
+import {ManifestReader} from "./ManifestReader";
 import {BuildContext, Entry, EntryDeps} from "./Interfaces";
 import {walkEntries, walkNonSubsetDeps} from "./Walkers";
 import {readFileFromHeadOrNow} from "./GitUtils";
